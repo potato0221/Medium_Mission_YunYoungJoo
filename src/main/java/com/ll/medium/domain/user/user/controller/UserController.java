d66c34775d821693950c9e4e9a1e74ff36535833
potato0221 <wnqo10@naver.com>
2023-12-05 14:41:49 +0900

    securifyConfig 추가, 클래스 별 애노테이션 추가

--- src/main/java/com/ll/medium/domain/user/user/controller/UserController.java
+++ src/main/java/com/ll/medium/domain/user/user/controller/UserController.java
@@ -1,5 +1,12 @@
 package com.ll.medium.domain.user.user.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/user")
 public class UserController {
 
 
--- src/main/java/com/ll/medium/domain/user/user/service/UserService.java
+++ src/main/java/com/ll/medium/domain/user/user/service/UserService.java
@@ -1,4 +1,15 @@
 package com.ll.medium.domain.user.user.service;
 
+import com.ll.medium.domain.user.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
 }
