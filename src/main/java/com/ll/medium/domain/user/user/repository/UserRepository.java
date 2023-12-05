18720908316c4bfff1ab817f9b5d6d545f8dc5a2
potato0221 <wnqo10@naver.com>
2023-12-05 14:44:00 +0900

    회원가입, user 가져오기 기능 추가

--- src/main/java/com/ll/medium/domain/user/user/repository/UserRepository.java
+++ src/main/java/com/ll/medium/domain/user/user/repository/UserRepository.java
@@ -3,7 +3,11 @@ package com.ll.medium.domain.user.user.repository;
 import com.ll.medium.domain.user.user.entity.SiteUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<SiteUser,Long> {
 
+
+    Optional<SiteUser> findByusername(String username);
 
 }
--- src/main/java/com/ll/medium/domain/user/user/service/UserService.java
+++ src/main/java/com/ll/medium/domain/user/user/service/UserService.java
@@ -1,9 +1,13 @@
 package com.ll.medium.domain.user.user.service;
 
+import com.ll.medium.domain.user.user.entity.SiteUser;
 import com.ll.medium.domain.user.user.repository.UserRepository;
+import com.ll.medium.global.exception.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -11,5 +15,24 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+
+
+    public SiteUser create(String username, String email, String password){
+        SiteUser user=new SiteUser();
+        user.setUsername(username);
+        user.setEmail(email);
+        user.setPassword(passwordEncoder.encode(password));
+        this.userRepository.save(user);
+        return user;
+    }
+    public SiteUser getUser(String username){
+        Optional<SiteUser> siteUser=this.userRepository.findByusername(username);
+        if(siteUser.isPresent()){
+            return siteUser.get();
+        }else {
+            throw new DataNotFoundException("siteuser not found");
+        }
+    }
+
 
 }
