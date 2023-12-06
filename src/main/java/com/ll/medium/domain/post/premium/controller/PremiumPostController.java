6cbdb60d4c0cfb879046f36d74f4e93a4acde228
potato0221 <wnqo10@naver.com>
2023-12-06 11:24:50 +0900

    멤버십 글 작성, html 분리

--- src/main/java/com/ll/medium/domain/post/premium/controller/PremiumPostController.java
+++ src/main/java/com/ll/medium/domain/post/premium/controller/PremiumPostController.java
@@ -2,13 +2,19 @@ package com.ll.medium.domain.post.premium.controller;
 
 import com.ll.medium.domain.member.member.entity.SiteMember;
 import com.ll.medium.domain.member.member.service.MemberService;
+import com.ll.medium.domain.post.premium.dto.PremiumPostForm;
 import com.ll.medium.domain.post.premium.entity.PremiumPost;
 import com.ll.medium.domain.post.premium.service.PremiumPostService;
+import com.ll.medium.global.rq.Rq;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -21,13 +27,14 @@ public class PremiumPostController {
 
     private final PremiumPostService premiumPostService;
     private final MemberService memberService;
+    private final Rq rq;
 
     @GetMapping("/post/list")
     public String list(Model model,
                        @RequestParam(value="page",defaultValue = "0") int page){
         Page<PremiumPost> paging=this.premiumPostService.getList(page);
         model.addAttribute("paging",paging);
-        return "post/post/post_list";
+        return "post/premium/premium_post_list";
     }
 
 
@@ -40,7 +47,25 @@ public class PremiumPostController {
         SiteMember siteMember = this.memberService.getUser(principal.getName());
         Page<PremiumPost> paging=this.premiumPostService.getListByUsername(page,siteMember);
         model.addAttribute("paging",paging);
-        return "/post/post/post_list";
+        return "post/premium/premium_post_list";
     }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/post/create")
+    public String postCreate(PremiumPostForm premiumPostForm){
+        return "post/premium/premium_post_form";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/post/create")
+    public String postCreate(@Valid PremiumPostForm premiumPostForm,
+                             BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "post/premium/premium_post_form";
+        }
+        this.premiumPostService.create(premiumPostForm.getTitle(),premiumPostForm.getContent(),rq.getMember());
+        return "redirect:/premium/post/list";
+    }
+
 
 }
